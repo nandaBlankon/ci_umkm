f7577ebf142ba9fbb3af4d53dd5e38e0e9cd9e86
unknown <teukunandasaputra@gmail.com>
2022-12-12 21:11:26 +0700

    Menambahkan kata data di header file laporanTransaksiPenjual.php

--- application/views/admin/laporanTransaksiPenjual.php
+++ application/views/admin/laporanTransaksiPenjual.php
@@ -6,7 +6,7 @@
     <div class="card shadow mb-4">
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">
-                LAPORAN KESELERUHAN TRANSAKSI PENJUAL
+                LAPORAN KESELERUHAN DATA TRANSAKSI PENJUAL
             </h6>
         </div>
         <div class="card-body">
